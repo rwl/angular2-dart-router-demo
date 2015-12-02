4335ff467c7ed5f0c33f61b5765bc423b1fbb071
Richard Lincoln <r.w.lincoln@gmail.com>
2015-12-02 21:49:44 +0100

    Adding child routes with route parameters.

--- web/main.dart
+++ web/main.dart
@@ -7,23 +7,70 @@ import 'package:angular2/bootstrap.dart';
     template: '''
 <h1>Articles</h1>
 <ul>
-<li><a [router-link]="[ '/Article', {articleId: 'article1'} ]">Article 1</a>
+<li><a [router-link]="[ '/Article/View', {articleId: 'article1'} ]">Article 1</a>
+<!-- <li><a (click)="onClick()">Article 1</a> -->
 </ul>
 ''',
     directives: const [ROUTER_DIRECTIVES])
-class ArticleList {}
+class ArticleList {
+  Router router;
+  ArticleList(this.router);
+  onClick() {
+//    router.navigateByUrl('/article/article1/view');
+    var inst = router.generate([
+      '/Article/View',
+      {'articleId': 'article1'}
+    ]);
+    print(inst.component.params);
+    print(inst.component.urlParams);
+    print(inst.component.urlPath);
+//    router.navigate([
+//      '/Article/View',
+//      {'articleId': 'article1'}
+//    ]);
+  }
+}
 
 //
 //
 //
+
+@Component(selector: 'article-editor')
+@View(
+    template: '''
+<h1>Article Editor</h1>
+<a [router-link]="[ '/Article/View', {articleId: routeParams.get('articleId')} ]">View</a>
+''',
+    directives: const [ROUTER_DIRECTIVES])
+class ArticleEditor {
+  final RouteParams routeParams;
+  ArticleEditor(this.routeParams);
+}
+
+@Component(selector: 'article-view')
+@View(
+    template: '''
+<h1>Article View</h1>
+<a [router-link]="[ '/Article/Edit', {articleId: routeParams.get('articleId')} ]">Edit</a>
+''',
+    directives: const [ROUTER_DIRECTIVES])
+class ArticleView {
+  final RouteParams routeParams;
+  ArticleView(this.routeParams);
+}
 
 @Component(selector: 'article')
 @View(
     template: '''
 <h1>Article</h1>
 <a [router-link]="[ '/ArticleList' ]">View All</a>
+<router-outlet></router-outlet>
 ''',
     directives: const [ROUTER_DIRECTIVES])
+@RouteConfig(const [
+  const Route(path: '/:articleId/edit', name: 'Edit', component: ArticleEditor),
+  const Route(path: '/:articleId/view', name: 'View', component: ArticleView)
+])
 class Article {}
 
 //
@@ -42,7 +89,9 @@ class Article {}
 @RouteConfig(const [
   const Redirect(path: '/', redirectTo: '/articles'),
   const Route(path: '/articles', name: 'ArticleList', component: ArticleList),
-  const Route(path: '/:articleId', name: 'Article', component: Article)
+  const Route(path: '/article/...', name: 'Article', component: Article),
+//  const Route(
+//      path: '/article/:articleId/...', name: 'Article', component: Article)
 ])
 class Index {
   Router router;
@@ -56,7 +105,8 @@ class Index {
 
 main() {
   bootstrap(Index, [
-    ROUTER_PROVIDERS,
+    ROUTER_PROVIDERS
+//    , provide(APP_BASE_HREF, useValue: '/')
 //    provide(LocationStrategy, useClass: HashLocationStrategy)
   ]);
 }
